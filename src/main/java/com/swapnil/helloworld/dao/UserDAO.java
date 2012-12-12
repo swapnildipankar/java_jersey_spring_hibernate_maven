4deeccab21bbfcb1d5f762d2be791177d8c830e9
sdipankar <sdipankar@zynga.com>
2012-12-12 09:38:33 -0800

    Code for updating an existing user

--- src/main/java/com/swapnil/helloworld/dao/UserDAO.java
+++ src/main/java/com/swapnil/helloworld/dao/UserDAO.java
@@ -29,6 +29,38 @@ public class UserDAO {
 
     public User update(Long userID, User user) {
         System.out.println("UserDAO: update");
+        System.out.println("UserDAO: START - updating user to the database");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        User fetchedUser = fetch(userID);
+
+        if(user.getNameFirst() != null && !fetchedUser.getNameFirst().equals(user.getNameFirst())) {
+            fetchedUser.setNameFirst(user.getNameFirst());
+        }
+        if(user.getNameMiddle() != null && !fetchedUser.getNameMiddle().equals(user.getNameMiddle())) {
+            fetchedUser.setNameMiddle(user.getNameMiddle());
+        }
+        if(user.getNameLast() != null && !fetchedUser.getNameLast().equals(user.getNameLast())) {
+            fetchedUser.setNameLast(user.getNameLast());
+        }
+        if(user.getPassword() != null && !fetchedUser.getPassword().equals(user.getPassword())) {
+            fetchedUser.setPassword(user.getPassword());
+        }
+        if(user.getDateOfBirth() != null && fetchedUser.getDateOfBirth() != user.getDateOfBirth()) {
+            fetchedUser.setDateOfBirth(user.getDateOfBirth());
+        }
+        if(user.getMonthOfBirth() != null && fetchedUser.getMonthOfBirth() != user.getMonthOfBirth()) {
+            fetchedUser.setMonthOfBirth(user.getMonthOfBirth());
+        }
+        if(user.getYearOfBirth() != null && fetchedUser.getYearOfBirth() != user.getYearOfBirth()) {
+            fetchedUser.setYearOfBirth(user.getYearOfBirth());
+        }
+
+        session.update(fetchedUser);
+        session.getTransaction().commit();
+        System.out.println("UserDAO: END - updating user to the database");
+
         return user;
     }
 
